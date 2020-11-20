6f842c1dd2846aa0874263fc06d4ff788aab723d
JordanBrother <cool.fgh54@yandex.ru>
2020-11-20 21:34:47 +0300

    OK

---
+++ Lab№1ПпТП/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Lab_1ПпТП
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ArrayList myAL = new ArrayList();
+            int index;
+            int itemCount = Convert.ToInt32(tbN.Text);
+            Random rnd1 = new Random();
+            int number;
+            lbMain.Items.Clear();
+            for (index = 1; index <= itemCount; index++)
+            {
+                number = -100 + rnd1.Next(200);
+                myAL.Add(number);
+                lbMain.Items.Add(number);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ArrayList myAL = new ArrayList();
+            int index;
+            int itemCount = Convert.ToInt32(tbN.Text);
+            Random rnd1 = new Random();
+            int number;
+            lbMain.Items.Clear();
+            lbMain.Items.Add("Исходный массив");
+            for (index = 1; index <= itemCount; index++)
+            {
+                number = -100 + rnd1.Next(200);
+                myAL.Add(number);
+                lbMain.Items.Add(number);
+            }
+            myAL.Sort();
+            lbMain.Items.Add("Отсортированный массив");
+            foreach (int elem in myAL)
+            {
+                lbMain.Items.Add(elem);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog myDialog = new SaveFileDialog();
+            myDialog.Filter = "Текст(*.TXT)|*.TXT" + "|Все файлы (*.*)|*.* ";
+
+            if (myDialog.ShowDialog() == true)
+            {
+                string filename = myDialog.FileName;
+                using (StreamWriter file = new StreamWriter(filename, false))
+                {
+                    foreach (Object item in lbMain.Items)
+                    {
+                        file.WriteLine(item);
+                    }
+                }
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+
+
+
+
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            ArrayList myAL = new ArrayList();
+            int index, k = 0;
+            int itemCount = Convert.ToInt32(tbN.Text); // изменить эту конвертацию
+            Random rnd1 = new Random();
+            int number;
+            lbMain.Items.Clear();
+            for (index = 1; index <= itemCount; index++)
+            {
+                number = -100 + rnd1.Next(200);
+                myAL.Add(number);
+                lbMain.Items.Add(number);
+            }
+          
+            for (index = 2; index <= itemCount - 2; index++)  // для этой хуйни и ниже
+            {
+                int a = Convert.ToInt32(lbMain.Items[index]);
+                int b = Convert.ToInt32(lbMain.Items[index + 1]);
+                int c = Convert.ToInt32(lbMain.Items[index - 1]);
+                if (a > b && a > c) k++;
+            }
+            lbMain.Items.Add("Кол-во элементов, которые больше своих соседей:");
+            lbMain.Items.Add(k);
+        }
+
+
+
+
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            ArrayList myAL = new ArrayList();
+            int index;
+            int itemCount = Convert.ToInt32(tbN.Text); // для этой хуйни и ниже
+            Random rnd1 = new Random();
+            int number;
+            lbMain.Items.Clear();
+            for (index = 1; index <= itemCount; index++)
+            {
+                number = -100 + rnd1.Next(200);
+                myAL.Add(number);
+                lbMain.Items.Add(number);
+            }
+
+            for (index = 0; index <= itemCount + 1; index++) // для этой хуйни и ниже
+            {
+                int a = Convert.ToInt32(lbMain.Items[index]);
+                if (a > 25)
+                {
+                    lbMain.Items.Add("Первый элемент, который больше 25:"); // для этой хуйни и ниже
+                    lbMain.Items.Add(index + " = " + lbMain.Items[index]);
+                }
+                break;
+            }
+           
+        }
+    }
+}
